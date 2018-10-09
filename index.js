075233ce06295e3acf00e9acd9fe0615a9a84d64
David Espinoza <davidespinoza@att.net>
2018-10-09 05:03:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  string = "hello";
+  string = 'hello';
   if(string === string.toLowerCase()){
     return "I can't hear you!";
   }
