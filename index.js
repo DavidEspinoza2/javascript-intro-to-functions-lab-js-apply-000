aa369103506192d322ffe7879d98eb9d4777d500
David Espinoza <davidespinoza@att.net>
2018-10-09 05:09:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,9 +22,10 @@ function sayHiToGrandma(string){
     return "I can't hear you!";
   }
 }
-/*function sayHiToGrandma(string){
+
+function sayHiToGrandma(string){
   string = 'HELLO';
   if(string === string.toUpperCase()){
     return "YES INDEED!";
   }
-}*/
+}
